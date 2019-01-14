053d2f3c80b9b191f8542b031bda70f3b8c44efe
carolcesp <carolinacespedes90@gmail.com>
2019-01-14 09:49:13 +0100

    add at github pages

---
+++ docs/precache-manifest.807b45d876fd2c9b23f7fd2ba5a4dfb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c4cdb4927dcce84889f",
+    "url": "./static/css/main.fc3b8f6a.chunk.css"
+  },
+  {
+    "revision": "9c4cdb4927dcce84889f",
+    "url": "./static/js/main.9c4cdb49.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0035ecda986b119fe61f5a1bbc649309",
+    "url": "./index.html"
+  }
+];
